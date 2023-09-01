c6edc8727fc56eed33b27cec78b12463603bec74
JJ <jj.vega86@gmail.com>
2023-09-01 14:17:49 -0500

    Add top level component hierarchy

---
+++ src/app/song-form/song-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-song-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './song-form.component.html',
+  styleUrls: ['./song-form.component.css']
+})
+export class SongFormComponent {
+
+}
---
+++ src/app/song-table/song-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-song-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './song-table.component.html',
+  styleUrls: ['./song-table.component.css']
+})
+export class SongTableComponent {
+
+}
