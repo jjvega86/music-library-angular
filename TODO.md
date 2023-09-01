02ad3e9908f9f61faccdb82c884ba45aa6172ecf
JJ <jj.vega86@gmail.com>
2023-09-01 16:41:22 -0500

    Add TODOs and song form scaffolding

---
+++ TODO.md
@@ -0,0 +1,11 @@
+// TODO: Create a My JSON Server API for the backend https://my-json-server.typicode.com/
+// TODO: Spin up .NET Music Library API
+// TODO: Change table data to consume music service
+// TODO: Create a reactive form for adding a song
+// TODO: Wire form to API POST call and wire table to react to added songs
+// TODO: Create new columns for DELETE and EDIT 
+// TODO: Wire DELETE button on each row to delete correct song (table should react)
+// TODO: Wire EDIT button on each row to allow for editing song in place (on save, row and API shows updated data)
+// TODO: Complete design and styling
+// TODO: Create types/interfaces for all data structures (song, song-list, etc)
+
--- src/app/song-form/song-form.component.ts
+++ src/app/song-form/song-form.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-song-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './song-form.component.html',
   styleUrls: ['./song-form.component.css']
 })
 export class SongFormComponent {
+  songForm = new FormGroup({
+    name: new FormControl(""),
+    artist: new FormControl(""),
+    album: new FormControl(""),
+    genre: new FormControl(""),
+    releaseDate: new FormControl(""),
+  })
+
+ addSong(){
+  // TODO: make call to music service to POST song information
+
+ }
 
 }
